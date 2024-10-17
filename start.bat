b8534c08d47d023e2fa366aa5278c46026c6d678
hugo blayes <HugIII@hugo@blayes.fr>
2024-10-17 21:36:37 +0200

    py->python launch

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
-start /B py ./sandbox/Scorer/main.py Scorer Wi-Fi 5670
-start /B py ./sandbox/Engine/main.py Engine Wi-Fi 5670
-start /B py ./sandbox/Engine_Map/main.py Engine_Map Wi-Fi 5670
-start /B py ./sandbox/Ennemies/main.py Ennemies Wi-Fi 5670
-start /B py ./sandbox/Starter/main.py Starter Wi-Fi 5670
+start /B python ./sandbox/Scorer/main.py Scorer Wi-Fi 5670
+start /B python ./sandbox/Engine/main.py Engine Wi-Fi 5670
+start /B python ./sandbox/Engine_Map/main.py Engine_Map Wi-Fi 5670
+start /B python ./sandbox/Ennemies/main.py Ennemies Wi-Fi 5670
+start /B python ./sandbox/Starter/main.py Starter Wi-Fi 5670
