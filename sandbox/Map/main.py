a26d137e22d302046be57f63295c392aad44e320
Clara Baffogne <clara.baffogne@gmail.com>
2024-10-18 21:51:46 +0200

    ajout map

---
+++ sandbox/Map/main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env -P /usr/bin:/usr/local/bin python3 -B
+# coding: utf-8
+
+#
+#  main.py
+#  Map
+#  Created by Ingenuity i/o on 2024/10/18
+#
+
+import sys
+import ingescape as igs
+
+string_map = [["X","X","X","X","X","X","X","X","X","X"],
+              ["X",".",".",".",".",".",".",".",".","X"],
+              ["X",".",".",".",".",".",".",".",".","X"],
+              ["X",".",".",".",".",".",".",".",".","X"],
+              ["X",".",".",".",".",".",".",".",".","X"],
+              ["X",".",".","X","X","X",".",".",".","X"],
+              ["X",".",".",".",".",".",".",".",".","X"],
+              ["X",".",".",".",".",".",".",".",".","X"],
+              ["X",".",".",".",".",".",".",".",".","X"],
+              ["X","X","X","X","X","X","X","X","X","X"]]
+
+#inputs
+def input_callback(io_type, name, value_type, value, my_data):
+    if (name == "start"):
+        s = ""
+        for m in string_map:
+            for l in m:
+                s += l
+            s += "R"
+        print(s)
+        igs.output_set_string("map", s)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("usage: python3 main.py agent_name network_device port")
+        devices = igs.net_devices_list()
+        print("Please restart with one of these devices as network_device argument:")
+        for device in devices:
+            print(f" {device}")
+        exit(0)
+
+    igs.agent_set_name(sys.argv[1])
+    igs.log_set_console(True)
+    igs.log_set_file(True, None)
+    igs.set_command_line(sys.executable + " " + " ".join(sys.argv))
+
+    igs.debug(f"Ingescape version: {igs.version()} (protocol v{igs.protocol()})")
+
+    igs.input_create("start", igs.IMPULSION_T, None)
+
+    igs.output_create("map", igs.STRING_T, None)
+
+    igs.observe_input("start", input_callback, None)
+
+    igs.start_with_device(sys.argv[2], int(sys.argv[3]))
+
+    input()
+
+    igs.stop()
+
