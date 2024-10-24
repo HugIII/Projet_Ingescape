b79f4a87a8d13f30f2c8ef2db7c25f3056b030c4
Clara Baffogne <clara.baffogne@gmail.com>
2024-10-24 15:46:43 +0200

    BE_ID

--- start.bat
+++ start.bat
@@ -1,11 +1,11 @@
+start /B python ./sandbox/Engine_Map/main.py Engine_Map Wi-Fi 5670
 start /B python ./sandbox/Engine/main.py Engine Wi-Fi 5670
 start /B python ./sandbox/Scorer/main.py Scorer Wi-Fi 5670
 start /B python ./sandbox/Map/main.py Map Wi-Fi 5670
 start /B python ./sandbox/Ennemies/main.py Ennemies Wi-Fi 5670
 start /B python ./sandbox/Weapons/main.py Weapons Wi-Fi 5670
 start /B python ./sandbox/Screamer/main.py Screamer Wi-Fi 5670
-start /B python ./sandbox/Engine_Map/main.py Engine_Map Wi-Fi 5670
 start /B python ./sandbox/Starter/main.py Starter Wi-Fi 5670
-start /B python ./sandbox/EventKeyBoard/main.py EventKeyBoard Wi-Fi 5670
 start /B python ./sandbox/Player/main.py Player Wi-Fi 5670
+start /B python ./sandbox/EventKeyBoard/main.py EventKeyBoard Wi-Fi 5670
 start /B ./sandbox/whiteboard/Whiteboard.exe --device Wi-Fi --port 5670
